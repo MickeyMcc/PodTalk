8f21a31a1ed7dfa7e0cb8551fb3d357381bbd392
Marci McClenon <mccmarci@gmail.com>
2018-02-13 19:47:58 -0600

    "Style improvements?"

---
+++ react-client/src/app.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import actions from './actions.js';
+import ShowList from './components/ShowList.jsx';
+import SearchList from './components/SearchList.jsx';
+
+import {
+  addShow,
+  search,
+  reset,
+} from './actions';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      user: 'default',
+      shows: [],
+      searchResults: ['hey', 'we', 'are', 'results']
+    }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: '/shows', 
+      success: (data) => {
+        this.setState({
+          shows: data
+        })
+      },
+      error: (err) => {
+        console.log('err', err);
+      }
+    });
+  }
+
+  render () {
+    return (<div>
+      <h1 id = 'title' >PodStar</h1>
+      <nav className = 'nav-bar'> <ul>
+        <li> Hello {this.state.user} </li>
+        <li> Login </li>
+        <li> Sign Up </li>
+        <li> Log Out </li>
+      </ul> </nav>
+      <SearchList results={this.state.searchResults} />
+      <ShowList shows={this.state.shows}/>
+    </div>)
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    username: state.username,
+    shows: state.shows
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch: dispatch,
+    onAddShow: (show) => dispatch(addShow(show)),
+    onReset: () => dispatch(reset())
+  }
+}
+
+export default App;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,69 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import ShowList from './components/ShowList.jsx';
-import SearchList from './components/SearchList.jsx';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import App from './app.jsx';
 
-import {
-  addShow,
-  search,
-  reset,
-} from './actions';
+let store = createStore(
+  reducer,
+);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      user: 'default',
-      shows: [],
-      searchResults: ['hey', 'we', 'are', 'results']
-    }
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: '/shows', 
-      success: (data) => {
-        this.setState({
-          shows: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
-
-  render () {
-    return (<div>
-      <h1 id = 'title' >PodStar</h1>
-      <nav className = 'nav-bar'> <ul>
-        <li> Hello {this.state.user} </li>
-        <li> Login </li>
-        <li> Sign Up </li>
-        <li> Log Out </li>
-      </ul> </nav>
-      <SearchList results={this.state.searchResults} />
-      <ShowList shows={this.state.shows}/>
-    </div>)
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    username: state.username,
-    shows: state.shows
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    dispatch: dispatch,
-    onAddShow: (show) => dispatch(addShow(show)),
-    onReset: () => dispatch(reset())
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('app'));
